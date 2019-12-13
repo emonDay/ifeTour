1f009f2fe624b2bda574bd87099a4765925ed65f
Olamide Samuel <sam99kupo@gmail.com>
2019-12-13 22:37:37 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+
+const router = Router();
+
+/* GET index page. */
+router.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Ife Tour'
+  });
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact', {
+    title: 'Ife Tour || Contact'
+  });
+});
+
+module.exports = router;
